8a804092a3a0ea5d73fb8d990ce0976c48304e26
Felipe Concha <felipeconcha@MacBook-Pro-de-Felipe.local>
2018-06-02 19:43:36 +0200

    Este es el commit de ejemplo para el laboratorio 1 de clases de Driven security

---
+++ Ejemplo.R
@@ -0,0 +1,11 @@
+# This is my first R program
+print("Hello World!")
+
+subject   <- "primer proyecto Felipe"
+language  <- "R"
+print(paste("Este es el primer proyecto de felipe",
+            language,
+            "for ",
+            subject,
+            "subject",
+            sep = " "))
