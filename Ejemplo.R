2023dcc51120e4b9309f00ac46946b8a6c135f3b
Felipe Concha <felipeconcha@MacBook-Pro-de-Felipe.local>
2018-06-02 20:09:05 +0200

    test 3 commit

--- Ejemplo.R
+++ Ejemplo.R
@@ -1,7 +1,7 @@
 # This is my first R program
 print("Hello World!")
 
-subject   <- "primer proyecto Felipe1"
+subject   <- "primer proyecto Felipe"
 language  <- "R"
 print(paste("Este es el primer proyecto de felipe",
             language,
